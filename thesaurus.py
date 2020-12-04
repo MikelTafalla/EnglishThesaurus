d0e3067e31e41621dc253f49c101d8900e4f0a56
Mikel1991 <mikel362d@gmail.com>
2020-12-04 13:23:54 -0800

    adds message for non existing words

--- thesaurus.py
+++ thesaurus.py
@@ -5,7 +5,11 @@ data = json.load(open("data.json"))
 
 # Function to return meaning of input word
 def meaning(word):
-    return data[word]
+    # check for existing word
+    if word in data:
+        return data[word]
+    else:
+        return "Word doesn't exist. Please double check it."
 
 # Ask user to input word and store input in variable
 word = input("Enter word: ")
