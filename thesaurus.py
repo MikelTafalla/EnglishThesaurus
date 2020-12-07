d374caa7354851a753746618583a52e45c0d8716
Mikel1991 <mikel362d@gmail.com>
2020-12-07 13:59:28 -0800

    imports difflib to check word possibilities when misspelled

--- thesaurus.py
+++ thesaurus.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 # Load an open data.json file read mode
 data = json.load(open("data.json"))
@@ -6,20 +7,27 @@ data = json.load(open("data.json"))
 # Function to return meaning of input word
 def meaning(word):
     # change input word to lower case so there's no errors
-    word = word.lower()
+    wordL = word.lower()
     city = word.capitalize()
     acronym = word.upper()
-    
+
     # check for existing word
-    if word in data:
-        return data[word]
+    if wordL in data:
+        return data[wordL]
     elif city in data:
         return data[city]
     elif acronym in data:
         return data[acronym]
     else:
-        return "Word doesn't exist. Please double check it."
-
+        wGuessed = get_close_matches(word, data.keys())
+        wCorrected = input(f"did you mean {wGuessed[0]}?: Yes or No: ")
+        check = wCorrected.lower()
+        if check == "yes":
+            print(meaning(wGuessed[0]))
+        else:
+            word = input("Word doesn't exist. Please reenter your word:")
+            print(meaning(word))
+    return "thanks for using our dictionay"
 # Ask user to input word and store input in variable
 word = input("Enter word: ")
 
